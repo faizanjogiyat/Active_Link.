53e07814aab5a86b7ca5f9f2c12d0b838522918b
faizanjogiyat <faizijogiyat@gmail.com>
2024-02-02 14:25:20 +0500

    active nav bar

---
+++ script.js
@@ -0,0 +1,20 @@
+// let section = document.querySelectorAll('section')
+// let navlink = document.querySelectorAll('header nav a')
+
+// window.scroll= () => {
+//     section.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop -150;
+//         let heigth = sec.offsetHeight;
+//         let id =sec.getAttribute('id');
+
+//         if(top >= offset && top < offset + height){
+//             navlink.forEach(link => {
+//                 link.classList.remove('active');
+//                 document.querySelector('header nav a [href*=' + id + ']').classList.add
+//                 ('active');
+//             });
+//         };
+//     });
+
+// };
